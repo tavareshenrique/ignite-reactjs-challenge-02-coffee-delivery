09a6c0b28726a7e9926cfac1dbc44ac1f125de17
Henrique Tavares <ihenrits@gmail.com>
2022-09-09 21:39:05 -0300

    create Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
+import { ColorType } from "../../styles/themes/colors";
+
+import { ButtonContainer } from "./styles";
+
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  color: ColorType;
+}
+
+export function Button({ children, color = "purple", ...rest }: IButtonProps) {
+  return (
+    <ButtonContainer color={color} {...rest}>
+      {children}
+    </ButtonContainer>
+  );
+}
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { darken } from "polished";
+
+import { ColorType } from "../../styles/themes/colors";
+
+interface IButtonContainerStyleProps {
+  color: ColorType;
+}
+
+export const ButtonContainer = styled.button<IButtonContainerStyleProps>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  height: 40px;
+  padding: 10px;
+
+  background: ${(props) => props.theme[props.color]};
+  border: 0;
+  border-radius: 6px;
+
+  &:hover {
+    background: ${(props) => darken(0.1, props.theme[props.color])};
+  }
+`;
