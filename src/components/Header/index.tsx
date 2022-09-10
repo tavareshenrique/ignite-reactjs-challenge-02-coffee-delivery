6dbf3ef797fa2ccfa4116da3ec64fdf9fa4e062b
Henrique Tavares <ihenrits@gmail.com>
2022-09-09 21:39:23 -0300

    styling Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,35 @@
+import { MapPin, ShoppingCart } from "phosphor-react";
+
+import { Button } from "../../components/Button";
+
+import { HeaderContainer, HeaderContent } from "./styles";
+
+import coffeeDeliveryLogo from "../../../public/logo.svg";
+import { useEffect } from "react";
+
 export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <HeaderContainer>
+      <img
+        src={coffeeDeliveryLogo}
+        alt="Imagem de um copo de café roxo com um simbolo de um foguete e ao lado escrito Coffee Delivery"
+        width={84}
+        height={40}
+      />
+
+      <HeaderContent>
+        <Button color="purple-light">
+          <MapPin size={22} weight="fill" className="header__map-pin_icon" />
+          <span>Três Rios, RJ</span>
+        </Button>
+        <Button color="yellow-light">
+          <ShoppingCart
+            size={22}
+            weight="fill"
+            className="header__shopping-cart_icon"
+          />
+        </Button>
+      </HeaderContent>
+    </HeaderContainer>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,28 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Container = styled.div`
+export const HeaderContainer = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 
+  margin-bottom: 2rem;
+`;
+
+export const HeaderContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  button + button {
+    margin-left: 20px;
+  }
+
+  .header__map-pin_icon {
+    color: ${(props) => props.theme.purple};
+  }
+
+  .header__shopping-cart_icon {
+    color: ${(props) => props.theme["yellow-dark"]};
+  }
 `;
